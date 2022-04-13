6cfc6cdbe3a6ec22c94bd07662b3ddf84683a211
Oleg Kovalov <oleg@hey.com>
2022-04-13 10:20:34 +0200

    Add ReadContext and WriteContext

--- conn.go
+++ conn.go
@@ -1,11 +1,19 @@
 package netx
 
 import (
+	"context"
 	"errors"
 	"io"
 	"net"
 	"sync"
 )
+
+// Conn is a stream oriented network connection with i/o operations that are controlled by Contexts
+type CtxConn interface {
+	net.Conn
+	ReadContext(ctx context.Context, b []byte) (n int, err error)
+	WriteContext(ctx context.Context, b []byte) (n int, err error)
+}
 
 // Conn is a generic stream-oriented network connection with stats.
 //
@@ -16,6 +24,46 @@ type Conn struct {
 	net.Conn
 	stats     *Stats
 	closeOnce sync.Once
+}
+
+// ReadContext does same as Read method but with a context.
+// This method requires 1 additional goroutine from a worker pool.
+func (c *Conn) ReadContext(ctx context.Context, b []byte) (n int, err error) {
+	// TODO: bytes pool
+	buf := make([]byte, len(b))
+	// TODO: chan pool
+	ch := make(chan ioResult, 1)
+
+	// TODO: goroutine pool
+	go func() { ch <- newIOResult(c.Read(buf)) }()
+
+	select {
+	case res := <-ch:
+		copy(b, buf)
+		return res.n, res.err
+	case <-ctx.Done():
+		return 0, ctx.Err()
+	}
+}
+
+// WriteContext does same as Write method but with a context.
+// This method requires 1 additional goroutine from a worker pool.
+func (c *Conn) WriteContext(ctx context.Context, b []byte) (n int, err error) {
+	// TODO: bytes pool
+	buf := make([]byte, len(b))
+	copy(buf, b)
+	// TODO: chan pool
+	ch := make(chan ioResult, 1)
+
+	// TODO: goroutine pool
+	go func() { ch <- newIOResult(c.Write(buf)) }()
+
+	select {
+	case res := <-ch:
+		return res.n, res.err
+	case <-ctx.Done():
+		return 0, ctx.Err()
+	}
 }
 
 // Read reads data from the connection.
@@ -62,4 +110,13 @@ func (c *Conn) Close() error {
 		}
 	})
 	return err
+}
+
+type ioResult struct {
+	n   int
+	err error
+}
+
+func newIOResult(n int, err error) ioResult {
+	return ioResult{n: n, err: err}
 }
