8561e7bb2f73872cdbf894a0318aa1fdeae53472
Oleg Kovalov <oleg@hey.com>
2022-11-16 17:15:41 +0100

    Add WaitAddr and WaitPort

--- utils.go
+++ utils.go
@@ -1,11 +1,13 @@
 package netx
 
 import (
+	"context"
 	"errors"
 	"fmt"
 	"math/rand"
 	"net"
 	"os"
+	"time"
 )
 
 // EmptyPort looks for an empty port to listen on local interface.
@@ -18,6 +20,40 @@ func EmptyPort() (int, error) {
 		}
 	}
 	return 0, errors.New("cannot find available port")
+}
+
+// WaitPort until you port is free.
+func WaitPort(ctx context.Context, port int) error {
+	return WaitAddr(ctx, fmt.Sprintf(":%d", port))
+}
+
+// WaitAddr until you addr is free.
+func WaitAddr(ctx context.Context, addr string) error {
+	errCh := make(chan error, 1)
+	go func() {
+		for {
+			select {
+			case <-ctx.Done():
+				errCh <- fmt.Errorf("cannot connect to %s", addr)
+				return
+			default:
+				c, err := net.Dial("tcp", addr)
+				if err == nil {
+					c.Close()
+					errCh <- nil
+					return
+				}
+				time.Sleep(100 * time.Millisecond)
+			}
+		}
+	}()
+
+	select {
+	case <-ctx.Done():
+		return ctx.Err()
+	case err := <-errCh:
+		return err
+	}
 }
 
 // newError same as os.NewSyscallError but shorter.
